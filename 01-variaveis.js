44f10c96d309b63d41a3a08f31a7ff2d65f564ac
May <maysa.rondon@portalsesisp.org.br>
2024-02-07 11:26:00 -0300

    aprendendo comandos básicos do js

---
+++ 01-variaveis.js
@@ -0,0 +1,12 @@
+//Criando variáveis com "var" (var nome _variavel = <valor>;)
+
+var nome = "Maysa"; //Var cria variavel global
+var idade = 15;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+
+console.log("No dia 7 de junho terei", idade+1, "anos!");
+
+//aumentando automaticamente mais +1 (autoincremento)
+idade++ //idade = idade+1
+console.log("No dia 7 de junho terei", idade "anos!",)
---
+++ 02-aritimetico.js
@@ -0,0 +1,27 @@
+//Utilizando operadores aritimÉticos com JavaScript
+
+console.log("a soma entre 8 e 7 é:", 8 + 7);
+
+//efetuar a soma de dois números informados por você (var nome_variavel = 12)
+
+var x = 18;
+var y = 27;
+
+console.log("a soma de", x, "e", y, "é", x + y);
+
+var adicao = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("adicao=", adicao);
+console.log("subtracao", subtracao);
+console.log("multiplicacao=", multiplicacao);
+console.log("divisao=", divisao);
+
+//regras para nomear identificadores (var, const ou let) - podem começar com A-Z, a-z, _ ou $
+
+var MinhaVariavel = 10
+var OutraVariavel = 20
+const _minhaConstante = 3.1514
+var $minhaVariavel = 11
