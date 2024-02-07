064a1464ad5c4bbe623ad203f1ff7b64a9437def
May <maysa.rondon@portalsesisp.org.br>
2024-02-07 16:53:15 -0300

    soma de texto e operadores

---
+++ 03-somadetextos.js
@@ -0,0 +1,10 @@
+var nome = "Maysa";
+var boasvindas = "Olá " + nome + ", seja bem vinda.";
+
+console.log(boasvindas);
+
+var texto1 = "Olá";
+var texto2 = "Bom dia";
+
+console.log(texto1 + texto2);
+console.log(texto1 + ", " + texto2);
