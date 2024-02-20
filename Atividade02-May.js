1f061a8c6199d3adda72170cb35e2614d6458f95
May <maysa.rondon@portalsesisp.org.br>
2024-02-20 13:49:43 -0300

    realização e entrega da atividade :D

---
+++ Atividade02-May.js
@@ -0,0 +1,40 @@
+var nome = "Maysa"
+var ano = 2008
+var idade = 2024-ano;
+console.log("1.", nome, "nasceu no ano de", ano,"e no seu aniversário fará", idade, "anos !")
+
+console.log("")
+
+var comp = 12
+var larg = 6
+var area = larg*comp
+console.log("2. A área é de", area, "cm !")
+
+console.log("")
+
+var altu = 5
+var volu = area*altu
+console.log("3. O volume é de", volu, "cm³ !")
+
+console.log("")
+
+var a = 2
+var b = 3
+var c = -1
+var delta = b**2 -4 *a *c
+console.log("4. O valor de delta é", delta, "!")
+
+console.log("")
+
+var compra = 450
+var desconto = 3
+var porcentagem = desconto/100 *compra 
+var total = compra-porcentagem
+console.log("5. O valor final será", total, "reais !")
+
+console.log("")
+
+var velocidade = 95
+var tempo = 3.5
+var distancia = velocidade*tempo
+console.log("6. A distância percorrida foi de", distancia, "Km !")
