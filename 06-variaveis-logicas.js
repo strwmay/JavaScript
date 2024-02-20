1d406de8ed92483df003c186613e85f76ab5cfef
May <maysa.rondon@portalsesisp.org.br>
2024-02-20 08:40:39 -0300

    estudo de variaveis lógicas

---
+++ 06-variaveis-logicas.js
@@ -0,0 +1,13 @@
+// Variáveis lógicas (boolean)
+
+var nome = "Gustavo";
+var rico = false;
+var eProfessor = true;
+
+console.log("Nome:", nome, "| É professor?", eProfessor, "| É rico?", rico);
+console.log("-------------------------------------------------");
+
+var nome = "Maysa";
+var jogadora = false;
+
+console.log("- Olá meu nome é " + nome + " | É jogadora? " + jogadora);
