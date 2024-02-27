8230b36361c239743821730b87b42e5225ecde45
May <maysa.rondon@portalsesisp.org.br>
2024-02-27 14:25:27 -0300

    realização e entrega :)

--- Atividade03-May.js
+++ Atividade03-May.js
@@ -8,13 +8,23 @@ console.table(frutas);
 console.log("A fruta contida no índice 5 é", frutas[5]); // 4- mostrar fruta no indice 5
 
 frutas.splice(4, 1); // 5- remover uva
-// v outra forma de retirar v 
+// v outra forma de retirar v
 
 //var position = frutas.indexOf("Uva")
 //console.table(position, 1);
 
 console.table(frutas);
 
-var frutasCopia = frutas.slice(2, 5) // 6- criar cópia somente com frutas do indice 2, 3 e 4
+var frutasCopia = frutas.slice(2, 5); // 6- criar cópia somente com frutas do indice 2, 3 e 4
 
-console.table(frutasCopia); 
+console.table(frutasCopia);
+console.log("");
+
+var numeros = [1, 2, 3, 4, 5]; // 1- criar vetor com 5 elementos
+console.log(numeros[2]); // 2- mostrar terceiro elemento
+
+console.table(numeros);
+
+var numerosCopia = [1, 2, 3, 4, 5].map((number) => number * 2); // 3 e 4- cópia e dobro do valor
+
+console.table(numerosCopia);
