bc4f161ffdbb0e9c01118b9d0278f9636b7e83fd
May <maysa.rondon@portalsesisp.org.br>
2024-02-27 16:08:13 -0300

    realizando atividades

---
+++ Atividade04-May.js
@@ -0,0 +1,9 @@
+var matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]; // 1- criar matriz 3x3
+
+console.table(matriz);
+
+console.log("Os valores da diagonal principal são", matriz[0][0], matriz [1][1], matriz [2][2]) // 2- imprimir valores da diagonal principal
