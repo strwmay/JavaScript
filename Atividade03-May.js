7f92c6a37f023d308a78b1983bd727954b39ce8a
May <maysa.rondon@portalsesisp.org.br>
2024-02-27 16:04:16 -0300

    entrega da atividade :)

--- Atividade03-May.js
+++ Atividade03-May.js
@@ -33,3 +33,15 @@ numerosCopia [3] = numeros[3] *2
 numerosCopia [4] = numeros[4] *2
 
 console.table(numerosCopia)
+
+console.log("")
+
+var matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]; // 1- criar matriz 3x3
+
+console.table(matriz);
+
+console.log("Os valores da diagonal principal são", matriz[0][0], matriz [1][1], matriz [2][2]) // 2- imprimir valores da diagonal principal
